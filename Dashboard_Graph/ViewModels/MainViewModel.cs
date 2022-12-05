05f26494f2cad0d2dd29f6230aa17318e9da0f18
Hayden Lee <seaeast2@users.noreply.github.com>
2022-12-05 13:52:31 +0900

    Added another dashboard ui.

---
+++ Dashboard_Graph/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Dashboard_Graph.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+
+        //-> Commands
+        public ICommand? ShowHomeViewCommand { get; }
+        public ICommand? ShowCustomerViewCommand { get; }
+    }
+}
